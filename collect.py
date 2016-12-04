14ec27b8a9a77d9382e63bdb8faa033461db9905
kendricktan <kendricktan0814@gmail.com>
2016-12-04 11:18:02 +1000

    ✎ added settings

--- collect.py
+++ collect.py
@@ -1,4 +1,5 @@
 import cv2
+import json
 
 import numpy as np
 import pyscreenshot as ImageGrab
@@ -8,6 +9,11 @@ from iolistener import KeyBoardEventListener, MouseClickEventListener
 print('--- Endless Run Neural Network Approach ---')
 print('[X] Press "q" to quit')
 print('[!] Initializing...')
+
+# Settings
+with open('config.json', 'r') as f:
+    SETTINGS = json.load(f)
+
 print('[!] Ensure that the game window is initialized before proceeding')
 print('[!] Please click the top left and bottom right of the game window, and leave some margins')
 
@@ -21,11 +27,12 @@ mouseevents.start()
 while len(mouseevents.clicked_positions) < 2:
     pass
 
+''' ROI window calibration '''
 # ROI for game window
-roi_game = tuple([i for sub in mouseevents.clicked_positions for i in sub])
+ROI_GAME = [i for sub in mouseevents.clicked_positions for i in sub]
 
 # Grab screenshot of image
-img = ImageGrab.grab(bbox=roi_game)
+img = ImageGrab.grab(bbox=tuple(ROI_GAME))
 img = np.array(img)
 img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
@@ -38,16 +45,25 @@ ret, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 # Keep only largest contour and crop image to the ROI
+# Goal is to get the ROI
 cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
 roi_x, roi_y, roi_w, roi_h = cv2.boundingRect(cnts)
-img_roi = img[roi_y:roi_y + roi_h, roi_x:roi_x + roi_w]
+
+# Uncomment below to debug
+# img_roi = img[roi_y:roi_y + roi_h, roi_x:roi_x + roi_w]
+# cv2.imshow('img', img_roi)
+# cv2.waitKey(0)
+raw_input('[!] Calibration complete, input anything to continue')
+print('[!] Press "q" to quit')
+keyevents.end = False
 
 # Rescale image to 85 x 145 (width, height)
-img = cv2.resize(img_roi, (85, 145), interpolation=cv2.INTER_CUBIC)
-
-# Show window
-cv2.imshow('Game Window', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+while not keyevents.end:
+    img = ImageGrab.grab(bbox=tuple(ROI_GAME))
+    img = np.array(img)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    img_roi = img[roi_y:roi_y + roi_h, roi_x:roi_x + roi_w]
+    img = cv2.resize(img_roi, (SETTINGS['scaledx'], SETTINGS['scaledy']), interpolation=cv2.INTER_CUBIC)
 
 print('[X] Quitted')
+
